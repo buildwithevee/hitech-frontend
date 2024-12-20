b54a7f689d1bbeee5b11515d92a831ef7cbaeee9
ajshan23 <ajmalshahan23@gmail.com>
2024-12-21 02:31:57 +0530

    added dist

---
+++ dist/assets/Notifications-BtAufb5Y.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-8zyf2noZ.js";function o(){return i.jsx("div",{children:"Notifications"})}export{o as default};
---
+++ dist/assets/constant-ChE6BXXD.js
@@ -0,0 +1,1 @@
+const a="http://localhost:3000/api";export{a};
